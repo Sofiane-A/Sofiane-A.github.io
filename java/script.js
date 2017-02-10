f94e74e99aa3faf8682ca0a0b535c76635bab2d5
Sofiane-A <sofiane.ayache@devinci.fr>
2017-02-10 21:37:41 +0100

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,3 +1,7 @@
+function homePageLoading() {
+    url = 'http://bitcoin.mubiz.com/blockchaininfo';
+    elementID = 'https://Sofiane-A.github.io/bitcoin/index.html';
+    fonctionRequeteApi(url, elementID);
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
