ab68e3fd47a1a963905e698812185fdb9f2b0537
Sofiane-A <sofiane.ayache@devinci.fr>
2017-04-28 19:57:45 +0200

    Update action.php

--- action.php
+++ action.php
@@ -1,3 +1,5 @@
+<?php
 <script src="https://Sofiane-A.github.io/java/script.js"></script>
 <script type="text/javascript">homePageLoading($_POST['Requete'],'https://sofiane-a.github.io/bitcoin/explorer/index.html');</script>
 href="action.php"
+?>
