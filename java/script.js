7a1107a05b2b823677f032af4c46e52e5287fb7f
Sofiane-A <sofiane.ayache@devinci.fr>
2017-04-28 18:30:57 +0200

    Update script.js

--- java/script.js
+++ java/script.js
@@ -8,7 +8,7 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("demo").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send();
 }
 
