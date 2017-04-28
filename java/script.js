58b675af879892f15140d10ee7717de2a97774b1
Sofiane-A <sofiane.ayache@devinci.fr>
2017-04-28 17:32:27 +0200

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,15 +1,19 @@
-
-window.homePageLoading = function () {
-     url = "https://bitcoin.mubiz.com/blocks";
-    elementID = "https://sofiane-a.github.io/bitcoin/index.html";
-    var xmlhttp = new XMLHttpRequest();
+ function requeteApi(url,elementID)
+{
+ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById(elementID).innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET",url, true);
 xmlhttp.send();
+}
+
+function homePageLoading() {
+    url = 'https://bitcoin.mubiz.com/blocks';
+    elementID = 'https://sofiane-a.github.io/bitcoin/index.html';
+    fonctionRequeteApi(url, elementID);
 }
